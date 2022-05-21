585aa57685d032beb7636755d80e45fe94582806
Cta991 <74148197+Cta991@users.noreply.github.com>
2022-05-21 16:13:53 +0700

    Add files via upload

---
+++ Pdf convert.py
@@ -0,0 +1,24 @@
+import fitz
+import re
+doc = fitz.open(r'C:\Users\user\Desktop\Test.pdf')
+
+page1 = doc[0]
+words1 = page1.get_text('words')
+content1 = ' '.join([word[4] for word in words1])
+
+date = re.findall('[0-9]*/[0-9]*/[0-9]*', content1)[0]
+print('Date:', date)
+
+
+
+'''#print(content1)
+#print('================')
+
+page3 = doc[2]
+words2 = page3.get_text('words')
+content2 = ' '.join([word[4] for word in words2]).split()
+#print(content2)
+
+#matchContent = ' '.join([word for word in content1 if word in content2])
+#print(matchContent)
+'''
